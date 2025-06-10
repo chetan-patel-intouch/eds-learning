4daac4ba4350ee3b9bcb73ce1ccc9fc6e5d7c314
Chetan Patel <chetan.patel@intouchsol.com>
2025-06-10 21:03:40 +0530

    added table js

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -19,7 +19,6 @@ export default async function decorate(block) {
   if (header) table.append(thead);
   table.append(tbody);
 
-  
   [...block.children].forEach((child, i) => {
     const row = document.createElement('tr');
     if (header && i === 0) thead.append(row);
